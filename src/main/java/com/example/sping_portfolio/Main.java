41dae32f39f5bc37bc4f22efbb2b6736a77f1c9f
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-09-07 09:38:14 -0700

    code code code

--- src/main/java/com/example/sping_portfolio/Main.java
+++ src/main/java/com/example/sping_portfolio/Main.java
@@ -2,6 +2,8 @@ package com.example.sping_portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Main {
@@ -10,4 +12,31 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+}
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+class Aboutus {
+    @GetMapping("/aboutus")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String aboutus() {
+        return "aboutus"; // returns HTML VIEW (greeting)
+    }
+}
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+class Recipes {
+    @GetMapping("/recipes")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String recipes() {
+        return "recipes"; // returns HTML VIEW (greeting)
+    }
+}
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+class ava {
+    @GetMapping("/ava")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String ava() {
+        return "ava"; // returns HTML VIEW (greeting)
+    }
 }
