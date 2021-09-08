4ab2279e0bfcd2570513fe6b9d063b21ada928a9
Sarah Xie <sarahwxie@gmail.com>
2021-09-08 10:22:57 -0700

    fortune

--- src/main/java/com/example/sping_portfolio/Main.java
+++ src/main/java/com/example/sping_portfolio/Main.java
@@ -3,7 +3,9 @@ package com.example.sping_portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class Main {
@@ -40,3 +42,16 @@ class ava {
         return "ava"; // returns HTML VIEW (greeting)
     }
 }
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+class fortune {
+    @GetMapping("/fortune")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String fortune(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name);
+        return "fortune"; // returns HTML VIEW (greeting)
+    }
+}
+
+
