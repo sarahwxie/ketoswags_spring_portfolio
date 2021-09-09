f0b7d8c602cb1ab6b45f5a0868eb94c34feff316
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-09-09 10:03:39 -0700

    binary moment

--- src/main/java/com/example/sping_portfolio/Main.java
+++ src/main/java/com/example/sping_portfolio/Main.java
@@ -14,61 +14,46 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-}
+    @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+    public class MainController {
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-class Aboutus {
-    @GetMapping("/aboutus")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String aboutus() {
-        return "aboutus"; // returns HTML VIEW (greeting)
-    }
-}
+        @GetMapping("/aboutus")
+        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+        public String aboutus() {
+            return "aboutus"; // returns HTML VIEW (greeting)
+        }
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-class Recipes {
-    @GetMapping("/recipes")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String recipes() {
-        return "recipes"; // returns HTML VIEW (greeting)
-    }
-}
+        @GetMapping("/recipes")
+        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+        public String recipes() {
+            return "recipes"; // returns HTML VIEW (greeting)
+        }
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-class ava {
-    @GetMapping("/ava")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String ava() {
-        return "ava"; // returns HTML VIEW (greeting)
-    }
-}
+        @GetMapping("/ava")
+        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+        public String ava() {
+            return "ava"; // returns HTML VIEW (greeting)
+        }
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-class crystal {
-    @GetMapping("/crystal")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String crystal() {
-        return "crystal"; // returns HTML VIEW (greeting)
-    }
-}
+        @GetMapping("/crystal")
+        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+        public String crystal() {
+            return "crystal"; // returns HTML VIEW (greeting)
+        }
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-class sarah {
-    @GetMapping("/sarah")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String sarah() {
-        return "sarah"; // returns HTML VIEW (greeting)
-    }
-}
+        @GetMapping("/sarah")
+        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+        public String sarah() {
+            return "sarah"; // returns HTML VIEW (greeting)
+        }
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-class fortune {
-    @GetMapping("/fortune")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String fortune(@RequestParam(name="name", required=false, defaultValue="999") String name, Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        model.addAttribute("name", Integer.parseInt(name)+1);
-        return "fortune"; // returns HTML VIEW (greeting)
+        @GetMapping("/fortune")
+        // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+        public String fortune(@RequestParam(name = "name", required = false, defaultValue = "999") String name, Model model) {
+            // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+            model.addAttribute("name", Integer.parseInt(name) + 1);
+            return "fortune"; // returns HTML VIEW (greeting)
+        }
     }
 }
 
